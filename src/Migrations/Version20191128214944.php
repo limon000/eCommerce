83d877ef86adef20926f69177c23f7aa1f4e7b6e
AYMEN <mohamedaymen.radhouane@universitecentrale.tn>
2019-11-28 23:45:38 +0100

    stripe fix

--- src/Migrations/Version20191128214944.php
+++ src/Migrations/Version20191128214944.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191123220935 extends AbstractMigration
+final class Version20191128214944 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,7 @@ final class Version20191123220935 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, articles_id INT DEFAULT NULL, description VARCHAR(255) NOT NULL, quantity INT NOT NULL, status VARCHAR(255) NOT NULL, order_date DATETIME NOT NULL, order_total DOUBLE PRECISION NOT NULL, INDEX IDX_6EEAA67D19EB6921 (client_id), UNIQUE INDEX UNIQ_6EEAA67D1EBAF6CC (articles_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D19EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
-        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D1EBAF6CC FOREIGN KEY (articles_id) REFERENCES article (id)');
+        $this->addSql('ALTER TABLE commande DROP INDEX IDX_6EEAA67D1EBAF6CC, ADD UNIQUE INDEX UNIQ_6EEAA67D1EBAF6CC (articles_id)');
     }
 
     public function down(Schema $schema) : void
@@ -32,6 +30,6 @@ final class Version20191123220935 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE commande');
+        $this->addSql('ALTER TABLE commande DROP INDEX UNIQ_6EEAA67D1EBAF6CC, ADD INDEX IDX_6EEAA67D1EBAF6CC (articles_id)');
     }
 }
